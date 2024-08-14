94724a6286b8b2631179e465f9ec312bd3a21ffa
Nadezhda Chitakova <nkchitakova@lirex.com>
2024-08-14 15:49:42 +0300

    Created method to seed data at database

---
+++ GiftVote/Extensions/SeedDataExtension.cs
@@ -0,0 +1,49 @@
+﻿using Bogus;
+using GiftVote.Data.Context;
+using GiftVote.Data.Models;
+using Microsoft.AspNetCore.Builder;
+
+namespace GiftVote.Data.Extensions
+{
+    public static class SeedDataExtension
+    {
+        public static void SeedData(this IApplicationBuilder app)
+        {
+            var faker = new Faker();
+            using var context = new IdentityDbContext();
+            List<Gifts> gifts =
+            [
+                GiftFactory("Chocolate"),
+                GiftFactory("Parfume"),
+                GiftFactory("Jewel")
+            ];
+
+            List<Employee> employees = 
+            [
+                EmployeeFactory(faker.Person.FirstName, faker.Person.LastName, faker.Person.UserName),
+                EmployeeFactory(faker.Person.FirstName, faker.Person.LastName, faker.Person.UserName),
+                EmployeeFactory(faker.Person.FirstName, faker.Person.LastName, faker.Person.UserName),
+                EmployeeFactory(faker.Person.FirstName, faker.Person.LastName, faker.Person.UserName)
+                                       ];
+            context.Set<Gifts>()
+                .AddRange(gifts);
+
+        }
+
+        private static Gifts GiftFactory(string name)
+        => new()
+{
+Name = name
+};
+
+        private static Employee EmployeeFactory(string firstname, string lastname, string username)
+            => new()
+            {
+                FirstName = firstname,
+                LastName = lastname,
+                UserName = username,
+                Password = BCrypt.Net.BCrypt.HashPassword(username)
+            };
+    }
+    }
+
